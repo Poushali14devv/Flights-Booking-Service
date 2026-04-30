6dd96f2c7a5d67880f303cd46dc26129aa446ef2
Poushali14devv <poushalimajumder141102@gmail.com>
2026-04-30 23:32:51 +0530

    made proxy complaint

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,11 +1,27 @@
+// const express = require('express');
+
+// const { InfoController} = require('../../controllers');
+// const bookingRoutes=require('./booking')
+
+// const router = express.Router();
+
+// router.get('/info', InfoController.info);
+
+// router.use('/bookings',bookingRoutes);
+// module.exports = router;
+
+
+
 const express = require('express');
 
-const { InfoController} = require('../../controllers');
-const bookingRoutes=require('./booking')
+const { InfoController } = require('../../controllers');
+
+const bookingRoutes = require('./booking');
 
 const router = express.Router();
 
 router.get('/info', InfoController.info);
 
-router.use('/bookings',bookingRoutes);
+router.use('/bookings', bookingRoutes);
+
 module.exports = router;
